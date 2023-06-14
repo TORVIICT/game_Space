5369c97d0dd0785c102363d6f2a4478b9e30167c
Victor <victor.realpe@fundacion-jala.org>
2023-06-14 17:30:56 -0500

    nave

--- main.py
+++ main.py
@@ -3,56 +3,95 @@ from pygame.locals import *
 from random import randint
 
 
-
-pygame.init()
-
-ventana = pygame.display.set_mode((1400, 900))
-pygame.display.set_caption("GAME")
-
+#VARIABLES GLOBALES
+ancho = 900
+alto = 480
 color=(52,9,60)
-imagen=pygame.image.load("spaceship.png")
-imagen_trans = pygame.transform.scale(imagen, (60, 60)) #cambiar de tamaño nave
-#posX = randint(0,300) #ubicacion en X random
-posX = 700
-posY= 800
-
-velocidad = 10
-derecha = True
-
-rectangulo = pygame.Rect(0,0,100,50)
 
 
+class naveEspacial(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.ImagenNave = pygame.image.load('Imagenes/spaceship.png')
+        self.naveImage = pygame.transform.scale(self.ImagenNave, (60, 60)) #cambiar de tamaño nave
+
+        self.rect = self.naveImage.get_rect()
+        self.rect.centerx = ancho /2
+        self.rect.centery = alto - 60
+
+        self.listaDisparo = []
+        self.Vida = True
+
+        self.velocidad = 10
+
+    def movimiento(self):
+        if self.Vida == True:
+            if self.rect.left <= 0:
+                self.rect.left = 0 
+            elif self.rect.right >= ancho:
+                self.rect.right = ancho
+            
 
 
+    def disparar(self):
+        pass
+    def dibujar(self, superficie):
+        superficie.blit(self.naveImage,self.rect)
 
-while True:
-    ventana.fill(color) #rellena la ventana
-    ventana.blit(imagen_trans,(posX,posY))
-    pygame.draw.rect(ventana, (180,70,70), rectangulo )
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+def SpaceInvader():
+    pygame.init()
+    ventana = pygame.display.set_mode((ancho, alto))
+    pygame.display.set_caption("SpaceAttak")
 
+    ImagenFondo = pygame.image.load('Imagenes/fondo.jpg')
+
+    jugador = naveEspacial()
+
+    enJuego = True
+    while True:
+        jugador.movimiento()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if enJuego == True:
+                if event.type == pygame.KEYDOWN:
+                    if event.key ==K_LEFT:
+                        jugador.rect.left -= jugador.velocidad
+                    elif event.key == K_RIGHT:
+                        jugador.rect.right += jugador.velocidad
+                    elif event.key == K_UP:
+                        jugador.rect.y -= jugador.velocidad
+                    elif event.key == K_DOWN:
+                        jugador.rect.y += jugador.velocidad
+
+
+        ventana.blit(ImagenFondo, (0,0))
+        jugador.dibujar(ventana)
+        pygame.display.update()
+
+SpaceInvader()
 
 #Movimiento de nave con teclado
         #elif event.type == pygame.KEYDOWN:
-            if event.key ==K_LEFT:
-                posX -= velocidad
-            elif event.key == K_RIGHT:
-                posX += velocidad
-            elif event.key == K_UP:
-                posY -= velocidad
-            elif event.key == K_DOWN:
-                posY += velocidad
+                #if event.key ==K_LEFT:
+                   # posX -= velocidad
+                #elif event.key == K_RIGHT:
+                   #posX += velocidad
+                #elif event.key == K_UP:
+                    #posY -= velocidad
+                #elif event.key == K_DOWN:
+                    #posY += velocidad
 
 #Movimiento de nave con el mouse
-    posX, posY = pygame.mouse.get_pos()
+    #posX, posY = pygame.mouse.get_pos()
     #centrar mouse en la imagen
-    posX -= 30
-    posY -= 30
+    #posX -= 30
+    #posY -= 30
 
 
 
-    pygame.display.update()
+        
